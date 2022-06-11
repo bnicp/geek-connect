de94c803aa7c36a9077ca173c94b11ff0c833e36
christopherginn <hey.im.chris@live.com>
2022-06-11 14:09:04 -0400

    modal's toggle display when user clicks anywhere outside

--- public/js/login.js
+++ public/js/login.js
@@ -1,3 +1,5 @@
+// console.log("Hello sup")
+
 const loginFormHandler = async (event) => {
     event.preventDefault();
   
@@ -27,7 +29,8 @@ const loginFormHandler = async (event) => {
   .addEventListener('login', loginFormHandler);
 
   // Get the modal
-var modal = document.getElementById("id01");
+var modal1 = document.getElementById("id01");
+var modal2 = document.getElementById("id02");
 
 // Get the button that opens the modal
 var btn = document.getElementById("loginBtn");
@@ -40,14 +43,22 @@ btn.onclick = function() {
   modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  if (event.target == modal1) {
+    modal1.style.display = "none";
+  } else if (event.target == modal2) {
+    modal2.style.display ="none";
   }
 }
+
+// window.onclick = function(event) {
+//   if (event.target == modal2) {
+//     modal2.style.display = "none";
+//   }
+// }
