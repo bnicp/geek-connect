bffe814c7dc2e8bfde5cc9b393ce21b8b7969a8b
bnicp <bnp415@gmail.com>
2022-06-11 13:05:57 -0400

    updated home page with modals

--- public/js/login.js
+++ public/js/login.js
@@ -25,3 +25,29 @@ const loginFormHandler = async (event) => {
   document
   .querySelector('#login-form')
   .addEventListener('login', loginFormHandler);
+
+  // Get the modal
+var modal = document.getElementById("id01");
+
+// Get the button that opens the modal
+var btn = document.getElementById("loginBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
