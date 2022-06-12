496cdd7b5bede1ab1fb7f2de527aeb4e9f7620fb
Tori <torihendricks927@yahoo.com>
2022-06-12 11:24:04 -0500

    fixed login FINALLY

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -52,14 +52,14 @@ router.get('/', async (req, res) => {
 router.post('/', async (req, res) => {
   try {
     const userData = await User.create(req.body);
-    res.status(200).json(userData);
+    // res.status(200).json(userData);
     
-    // req.session.save(() => {
-    //   req.session.user_id = userData.id;
-    //   req.session.logged_in = true;
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
 
-    //   res.status(200).json(userData);
-    // });
+      res.status(200).json(userData);
+    });
   } catch (err) {
     res.status(400).json(err);
   }
