510c7348e8470c5812659446bae738748c7c8e8a
bnicp <bnp415@gmail.com>
2022-06-13 08:57:32 -0400

    new home layout and added image folders

--- public/js/login.js
+++ public/js/login.js
@@ -62,3 +62,29 @@ window.onclick = function(event) {
 //     modal2.style.display = "none";
 //   }
 // }
+
+// Accordion factions on signup page
+function myFunction(id) {
+  var x = document.getElementById(id);
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else {
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+
+// Slideshow on home
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1}
+  x[slideIndex-1].style.display = "block";
+  setTimeout(carousel, 3000); // Change image every 3 seconds
+}
