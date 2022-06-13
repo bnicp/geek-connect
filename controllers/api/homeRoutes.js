7fcc0f6f35c3599139797c24a7fef35643d781e1
christopherginn <hey.im.chris@live.com>
2022-06-13 08:23:50 -0400

    if user is logged in, they are won't be able to see home page, they are redirected to profile

--- controllers/api/homeRoutes.js
+++ controllers/api/homeRoutes.js
@@ -1,8 +1,11 @@
 const router = require('express').Router();
 // const { User } = require('../../models');
+const homeAuth = require('../../utils/homeauth');
 
-router.get('/', async (req, res) => {
-  res.render('home');
+router.get('/', homeAuth, async (req, res) => {
+  res.render('home', {
+    logged_in: req.session.logged_in
+  });
 });
 module.exports = router;
 
