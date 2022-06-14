e8ed47ff47aeffa1844d5bf64cdfad45794b793c
christopherginn <hey.im.chris@live.com>
2022-06-13 22:13:37 -0400

    GET route for profile that returns user data as well as users that share the same tags

--- controllers/api/profileRoutes.js
+++ controllers/api/profileRoutes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { filter } = require('lodash');
 const { User, Tag, UserTag, Category } = require('../../models');
 const withAuth = require('../../utils/auth.js');
 
@@ -32,15 +33,48 @@ const withAuth = require('../../utils/auth.js');
         include: [{ model: Tag, attributes: ['tag_name', 'id'], include: [{model: Category}] }],
       });
 
-      const tagData = await Tag.findAll({
-        where: { id: userData.tags[0].id },
-        include: [{ model: User, attributes: ['username'] }],
-      })
+      const tagData = []
+
+      for (let i=0; i < userData.tags.length; i++) {
+        const usertag = await Tag.findAll({
+          where: { id: userData.tags[i].id },
+          include: [{ model: User, attributes: ['username']}]
+        })
+        const userblah= usertag.map((user) => user.get({ plain: true}));
+        // const filterUsers = [];
+        for (let i=0; i < userblah[0].users.length; i++) {
+          if (userblah[0].users[i].username !== userData.username) {
+            tagData.push(userblah[0].users[i])
+          }
+        }
+        
+        // tagData.push(filterUsers);
+      }
+
+      // const usertag = await Tag.findAll({
+      //   where: { id: userData.tags[0].id },
+      //   include: [{ model: User, attributes: ['username']}]
+      // })
+      
+      // const userblah= usertag.map((user) => user.get({ plain: true}));
+      // res.json(userblah[0].users[1].username)
+      // console.log(userblah[0].users.length)
+
+      // res.json(tagData)
+      // console.log(userData)
+      // const tagData = await Tag.findAll({
+      //   where: { id: userData.tags[0].id },
+      //   include: [{ model: User, attributes: ['username'] }],
+      // })
+
+    
 
       // const users = userData.map((user) => user.get({ plain: true }));
       const users = userData.get({ plain: true });
       // const tags = tagData.get({ plain: true });
-      const tags = tagData.map((tag) => tag.get({ plain: true }));
+      // const tags = tagData.map((tag) => tag.get({ plain: true }));
+
+      // console.log(users)
 
       // res.json(tagData);
       // const resi = await res.json(tagData);
@@ -48,12 +82,11 @@ const withAuth = require('../../utils/auth.js');
       // console.log(resi)
       const obj = {};
       obj['user'] = users;
-      obj['sameTags'] = tags;
+      obj['sameTags'] = tagData;
       // res.json(obj)
 
       res.render('profile', {
         ...obj, 
-        // ...tags
         // logged_in: req.session.logged_in
       });
     } catch (err) {
